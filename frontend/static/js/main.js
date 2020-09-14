040766faf5f165375ba8a6ae592442017f90007b
Joseph <braidej@gmail.com>
2020-09-15 00:37:48 +0100

    mobile view changes

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
 
 // navigation bar toggle
 
-    // hide all shown 
+    // hide all shown on load
     $('#trade-portion').hide()
     $('#education-portion').hide()
     $('#about-portion').hide()
+    $('#nav-link-mobile').hide()
     
     // trade head toggle
     $('#trade-link').click(function(){
@@ -18,12 +19,30 @@ $(document).ready(function(){
     })
 
     // about portion
-
     $('#about-link').click(function(){
         $('#about-portion').toggle()
     })
-    // $('.btn-close').click(function(){
-    //     $('#trade-portion').hide()
-    //     console.log('hide')
-    // })
+
+    /* 
+       mobile nav bar 
+    */
+    $('#nav-toggle').click(function(){
+        $('#nav-link-mobile').toggle()
+    })
+
+    // sticky navigation 
+    $(window).scroll(function(){
+        // when the scroll  is 150px do the following
+        if ($(this).scrollTop() > 150 ){
+            // add sticky nav class to the custom nav bar
+            $('#custom-nav').addClass('sticky-nav');
+            
+            // hide all the open trade/analytics/about company that might be opened
+            // $('#trade-portion').hide()
+            // $('#education-portion').hide()
+            // $('#about-portion').hide()
+        } else {
+            $('#custom-nav').removeClass('sticky-nav')
+        }
+    })
 })
